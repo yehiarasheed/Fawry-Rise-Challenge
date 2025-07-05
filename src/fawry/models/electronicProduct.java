6fd464ff3932c5843d493a0067269d9590b78716
Yehia Rasheed <157399068+yehiarasheed@users.noreply.github.com>
2025-07-05 05:01:29 +0300

    Comments: Add the assumption that the weight is entered in grams.
    -As Objects of classes that implement the Shippable interface are created, their weights are entered as part of the shipping information so we assume that by default this weight is entered by grams.
    
    -This matches the challenge's description of making assumptions as needed and allows for more a more dynamic weight calculation, showing weights in grams (g) or kilograms (kg) as seen fit.

--- src/fawry/models/electronicProduct.java
+++ src/fawry/models/electronicProduct.java
@@ -3,6 +3,7 @@ package fawry.models;
 import fawry.Interfaces.Shippable;
 
 public class electronicProduct extends nonExpirableProduct implements Shippable {
+    //Assume Weight is in Grams
     private double weight;
 
     public electronicProduct(String name, double price, int quantity, double weight) {
--- src/fawry/models/groceryProduct.java
+++ src/fawry/models/groceryProduct.java
@@ -5,6 +5,7 @@ import fawry.Interfaces.Shippable;
 import java.time.LocalDate;
 
 public class groceryProduct extends expirableProduct implements Shippable {
+    // Assume Weight is in Grams
     private double weight;
 
     public groceryProduct(String name, double price, int quantity, LocalDate expirationDate, double weight) {
